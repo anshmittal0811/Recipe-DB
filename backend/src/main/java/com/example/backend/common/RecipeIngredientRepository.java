4a535eb8afed9665aef619d00a4e0688096e390b
Ansh Mittal <75305376+anshmittal0811@users.noreply.github.com>
2025-09-15 09:52:37 +0530

    Update RecipeIngredientRepository.java

--- backend/src/main/java/com/example/backend/common/RecipeIngredientRepository.java
+++ backend/src/main/java/com/example/backend/common/RecipeIngredientRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
@@ -41,4 +40,5 @@ public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredie
     LIMIT 20
 """, nativeQuery = true)
     List<IngredientRecipesResponse> findTop20RecipesByIngredient(@Param("ingredientId") Long ingredientId);
+
 }
