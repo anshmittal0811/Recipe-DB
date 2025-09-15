0b67ac1a71927344472b805a8546583916e6a3e7
Ansh Mittal <75305376+anshmittal0811@users.noreply.github.com>
2025-09-15 09:47:35 +0530

    Update IngredientRepository.java

--- backend/src/main/java/com/example/backend/ingredient/IngredientRepository.java
+++ backend/src/main/java/com/example/backend/ingredient/IngredientRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -59,4 +58,5 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     List<CategoryIngredientsResponse> findTop20ByCategory(@Param("category") String category);
 
     Optional<Ingredient> findById(Long id);
+    
 }
