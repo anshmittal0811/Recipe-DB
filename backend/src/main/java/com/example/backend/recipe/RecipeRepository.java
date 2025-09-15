2d162046930ec7663fe2eb946d65943fc2287b2d
Ansh Mittal <75305376+anshmittal0811@users.noreply.github.com>
2025-09-15 09:49:57 +0530

    Update RecipeRepository.java

--- backend/src/main/java/com/example/backend/recipe/RecipeRepository.java
+++ backend/src/main/java/com/example/backend/recipe/RecipeRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -117,4 +116,5 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
         ORDER BY r.region
     """, nativeQuery = true)
     List<RegionCalorieAverageResponse> getAverageCaloriesByRegion();
+
 }
